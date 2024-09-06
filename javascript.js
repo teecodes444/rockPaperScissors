2eb56463ea8b1622f9b8c68a63bdc01132301040
teecodes444 <171301131+teecodes444@users.noreply.github.com>
2024-09-06 15:27:23 -0500

    Add basic pseudocode to js file

---
+++ javascript.js
@@ -0,0 +1,9 @@
+//create a function that randomly returns the computer choice
+
+//create a function that takes user choice and return it
+
+//declare variables to track player score
+
+//write single round logic
+
+//write entire game logic
