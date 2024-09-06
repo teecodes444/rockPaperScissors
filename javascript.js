4977e810b07d71e118b34e4336d50e17be8882e0
teecodes444 <171301131+teecodes444@users.noreply.github.com>
2024-09-06 15:52:07 -0500

    Add getHumanChoice() function and conditional logic

--- javascript.js
+++ javascript.js
@@ -10,6 +10,27 @@ console.log(getComputerChoice());
 
 //create a function that takes user choice and return it
 
+function getHumanChoice() {
+    let uChoice = prompt("Choose: Rock, Paper, or Scissors").toLowerCase();
+    //save user choice in variable to be used in conditional statements
+    //use switch statements to validate user choice and return input
+    switch (uChoice) {
+        case 'rock':
+            return 'Rock';
+            break;
+        case 'paper':
+            return 'Paper';
+            break;
+        case 'scissors':
+            return 'Scissors';
+            break;
+        default:
+            return 'Not a valid choice.';
+    }
+}
+
+console.log(getHumanChoice());
+
 //declare variables to track player score
 
 //write single round logic
