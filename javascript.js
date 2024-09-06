c86bf95b88ba56bbea95a9985887e39a9744eb78
teecodes444 <171301131+teecodes444@users.noreply.github.com>
2024-09-06 15:36:29 -0500

    declared getComputerChoice() function and set random return using Math methods

--- javascript.js
+++ javascript.js
@@ -1,5 +1,13 @@
 //create a function that randomly returns the computer choice
 
+function getComputerChoice() {
+    const cChoice = Math.floor(Math.random() * 3) + 1;
+    // returns a random number between 1 and 3
+    return cChoice;
+}
+
+console.log(getComputerChoice());
+
 //create a function that takes user choice and return it
 
 //declare variables to track player score
